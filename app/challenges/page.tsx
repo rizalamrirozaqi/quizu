c4442ef894574b5362bf263ec0ef58767180e660
rushiaremka <rushiaremka@gmail.com>
2026-04-16 12:59:34 +0700

    challenges.tsx component optimization

--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -1,8 +1,11 @@
+import { Suspense } from "react";
 import { createClient } from "@/utils/supabase/server";
 import ChallengeFilters from "@/components/challenges/ChallengeFilters";
 import ChallengeList from "@/components/challenges/ChallengeList"; 
+import { Section } from "lucide-react";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
+export const revalidate = 60;
 
 type Props = {
   searchParams: Promise<{ q?: string; lang?: string }>;
@@ -31,31 +34,36 @@ export default async function ChallengesPage({ searchParams }: Props) {
   }
 
   return (
-    // 🔥 FIX BACKGROUND UTAMA: Abu Muda (Light) vs Hitam (Dark)
     <main className="min-h-screen bg-gray-50 dark:bg-black transition-colors duration-300">
       <div className="max-w-7xl mx-auto px-4 py-12">
         
         {/* Header Section */}
-        <div className="mb-8">
-          <h1 className="text-3xl font-bold mb-2 text-gray-900 dark:text-white transition-colors">
-            All Challenges
-          </h1>
-          <p className="text-gray-500 dark:text-zinc-400 transition-colors">
-            Explore, filter, and solve problems to increase your ELO.
-          </p>
-        </div>
+        <Suspense fallback={<SectionSkeleton title="All Challenges" />}>
+          <div className="mb-8">
+            <h1 className="text-3xl font-bold mb-2 text-gray-900 dark:text-white transition-colors">
+              All Challenges
+            </h1>
+            <p className="text-gray-500 dark:text-zinc-400 transition-colors">
+              Explore, filter, and solve problems to increase your ELO.
+            </p>
+          </div>
+        </Suspense>
 
         {/* Filters */}
-        <div className="mb-8">
-            <ChallengeFilters />
-        </div>
+        <Suspense fallback={<SectionSkeleton title="Challenge Filters" />}>
+          <div className="mb-8">
+              <ChallengeFilters />
+            </div>
+        </Suspense>
 
         {/* List Card */}
-        <ChallengeList 
-          initialData={initialChallenges || []} 
-          solvedIds={Array.from(solvedIds)} 
-          searchParams={params}
-        />
+        <Suspense fallback={<SectionSkeleton title="Challenges" />}>
+          <ChallengeList 
+            initialData={initialChallenges || []} 
+            solvedIds={Array.from(solvedIds)} 
+            searchParams={params}
+          />
+        </Suspense>
       </div>
     </main>
   );
